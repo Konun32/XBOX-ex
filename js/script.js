bb28c20570c970338cfc2ebea863eb14f5afcded
Konun32 <aleksey.u.work@mail.ru>
2022-07-18 22:25:08 +0500

    combining 2 pages and simplifying the phone menu

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ if (iconMenu) {
 		menuBody.classList.toggle('_active');
 		const Lock = document.querySelector('._lock');
 		if (Lock) {
-			menuList.insertAdjacentHTML('afterbegin', '<li class="_menu__person menu__item"><a href="#" class="menu__link"><img src="img/header/Shape.svg" alt=""><div class="menu__text">My XBOX</div><div class="menu__arrow"></div><ul class="menu__sub-list"><li><a href="#" class="menu__sub-link">chapter №1</a></li><li><a href="#" class="menu__sub-link">chapter №2</a></li><li><a href="#" class="menu__sub-link">chapter №3</a></li></ul></a></li>');
+			//menuList.insertAdjacentHTML('afterbegin', '<li class="_menu__person menu__item"><a href="#" class="menu__link"><img src="img/header/Shape.svg" alt=""><div class="menu__text">My XBOX</div><div class="menu__arrow"></div><ul class="menu__sub-list"><li><a href="#" class="menu__sub-link">chapter №1</a></li><li><a href="#" class="menu__sub-link">chapter №2</a></li><li><a href="#" class="menu__sub-link">chapter №3</a></li></ul></a></li>');
 			let menuArrows = document.querySelectorAll('.menu__arrow');
 			let menuItems = document.querySelectorAll('.menu__item');
 			if (menuArrows.length > 0) {
@@ -31,12 +31,12 @@ if (iconMenu) {
 					});
 				}
 			}
-		} else {
+		} /*else {
 			const menuPerson = document.querySelector('._menu__person');
 			if (menuPerson) {
 				menuPerson.remove();
 			}
-		}
+		}*/
 	})
 }
 
